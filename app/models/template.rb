d28e0b43468be439b1d06ec4e2fbae4d0dc1e8a1
Dagliana Farias <dagliana.farias14@gmail.com>
2022-04-08 16:54:35 -0400

    ajustes em layout

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,7 +1,7 @@
 class Template < ApplicationRecord
   has_many :template_questions, dependent: :destroy
   accepts_nested_attributes_for :template_questions, allow_destroy: true, reject_if: :all_blank
-  validates :template_questions, length: {minimum:1, maximum: 10, message: ": Deve ter no mínimo 1 e no máximo 10 questões"}
+  validates :template_questions, length: {minimum:1, maximum: 3, message: ": Deve ter no mínimo 1 e no máximo 10 questões"}
 
   validates :name, uniqueness: { case_sensitive: false }
   validates :name, presence: true
