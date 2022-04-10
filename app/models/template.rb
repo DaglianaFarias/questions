95926f4c41a9a1cae3ab5e7295c9032e2fcac2aa
Dagliana Farias <dagliana.farias14@gmail.com>
2022-04-10 19:04:18 -0400

    correcao em routes e mudanca em quantidade de questions

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,8 +1,18 @@
 class Template < ApplicationRecord
   has_many :template_questions, dependent: :destroy
   accepts_nested_attributes_for :template_questions, allow_destroy: true, reject_if: :all_blank
-  validates :template_questions, length: {minimum:1, maximum: 10, message: ": Deve ter no mínimo 1 e no máximo 10 questões"}
-
-  validates :name, uniqueness: { case_sensitive: false }
+  # validates :template_questions, length: {minimum:1, maximum: 10, message: ": Deve ter no mínimo 1 e no máximo 10 questões"}
   validates :name, presence: true
+  validates :name, uniqueness: { case_sensitive: false }
+  validate :maximum_and_minimum_questions
+
+
+  def maximum_and_minimum_questions
+    total_questions = self.template_questions.reject(&:marked_for_destruction?).size
+    if total_questions > 10
+      self.errors.add(:base, "Deve ter no máximo 10 questões!")
+    elsif total_questions < 1
+      self.errors.add(:base, "Template deve ter no mínimo 1 questão!")
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'templates#index'
   resources :templates
-  resources :template_questions
+  # resources :template_questions
 
   get '/templates/validate_name/:name', to: 'templates#validate_name'
 end
